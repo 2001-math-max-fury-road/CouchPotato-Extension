d4ad108aef902763feb78bc83b21f7c67a3b0647
Dani <dani.meyer.nd@gmail.com>
2020-03-30 19:50:28 -0400

    chore: more dom manipulation

--- popup.js
+++ popup.js
@@ -1,12 +1,19 @@
-let hulu = document.getElementById('dash-player-container')
+const huluPlayer = document.getElementById('dash-player-container');
+const huluParent = document.getElementsByClassName(
+  'Player__container addFocus'
+)[0];
+
 // create element, then append
 // use flexbox on const hulu website to render in thirds
 const div = document.createElement('DIV'); //<div><div/>
 div.setAttribute('id', 'couchPotato');
 const frame = document.createElement('iframe'); //<iframe></iframe>
 frame.setAttribute('src', 'http://couch-potato-extension.herokuapp.com/'); //<iframe src="...">
-frame.style.width='50%'
-hulu.style.width='50%'
 div.appendChild(frame); //frame belongs to div
-hulu.appendChild(div); //div belongs to hulu
-
+div.style.position = 'absolute';
+div.style.height = '100%';
+div.style.width = '50%';
+div.style.boxShadow = 'border-box';
+huluPlayer.style.width = '50%';
+huluParent.appendChild(div);
+// hulu.appendChild(div); //div belongs to hulu
